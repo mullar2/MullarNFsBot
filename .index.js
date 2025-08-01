a24bfa7aed304aef558522cbf805595f2e29c69e
Ray Mullar <mullarhudson@gmail.com>
2025-08-01 11:57:47 +0300

    Update .index.js

--- .index.js
+++ .index.js
@@ -1,38 +1,15 @@
-Here are the basic files you need to upload for your WhatsApp bot:
+js
+} else if (msg.includes(".menu")) {
+  reply = `📋 *Mullar-NFS Bot Menu*:
+  
+  🎵 .play song [name] – Search for a song  
+  ⚽ .player [name] – Info on a football player  
+  📰 .news – Latest news  
+  🎬 .movies – New movies  
+  📈 .live scores – Sports updates  
+  👤 .owner – Bot owner info  
+  💡 .help – See all commands`;
+}
 
----
-
-*1. `index.js`*
-
-```js
-const { Client } = require("whatsapp-web.js");
-const qrcode = require("qrcode-terminal");
-require("dotenv").config();
-
-const client = new Client();
-
-client.on("qr", qr => {
-  qrcode.generate(qr, { small: true });
-});
-
-client.on("ready", () => {
-  console.log("Mullar-NFS Bot is ready!");
-});
-
-client.on("message", async message => {
-  if (message.body === ".hi") {
-    message.reply("Hi! I'm Mullar-NFS, your WhatsApp bot.");
-  }
-
-  if (message.body.startsWith(".play song")) {
-    const song = message.body.replace(".play song ", "");
-    message.reply(`Searching for "${song}"... (feature coming soon)`);
-  }
-});
-
-client.initialize();
-```
-
----
 
 
